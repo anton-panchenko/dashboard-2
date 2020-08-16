592d48e17d3b9fea5a6f9ff89acbd648a864d70e
anton-panchenko <anton.panchenko.0832@gmail.com>
2020-08-16 19:40:00 +0300

    Finished charts

--- app/js/charts.js
+++ app/js/charts.js
@@ -1,86 +1,88 @@
-//------------------------------------------------------------------
-//--------------------------board_donut__chart----------------------
-//------------------------------------------------------------------
+document.addEventListener('DOMContentLoaded', function () {
 
-let ctx1 = document.getElementById('board_donut__chart').getContext('2d');
-let playerStatistics1 = new Chart(ctx1, {
-    type: 'doughnut',
-    data: {
-        datasets: [
-            {
-                backgroundColor: ['#6F52ED', '#FFB800', '#FF4C61', '#33D69F'],
-                data: [20, 20, 15, 7],
-                borderWidth: 0
+    let columnChart = Highcharts.chart('columnChart', {
+        chart: {
+            type: 'column',
+            spacingBottom: 0,
+            spacingTop: 8,
+            spacingLeft: 0,
+            spacingRight: 0,
+            style: {
+                fontFamily: 'Gilroy, Helvetica, Arial, sans-serif',
             }
-        ]
-    },
-    options: {
-        responsive: true,
-        cutoutPercentage: 85,
-        tooltips: {
-            enabled: false,
         },
-        title: {
-            display: false
-        }
-    }
-});
-
-//------------------------------------------------------------------
-//-------------------board_barChart__chart--------------------------
-//------------------------------------------------------------------
-
-let ctx2 = document.getElementById('board_barChart__chart').getContext('2d');
-let activePlayers = new Chart(ctx2, {
-    type: 'bar',
-    data: {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Sep', 'Oct', 'Nov', 'Dec'],
-        datasets: [{
-            data: [19, 15, 24, 8, 11, 10, 18, 19, 12, 17, 23],
-            backgroundColor: '#6F52ED',
-            // barThickness: 0.5,
-            // borderColor: '#3A8282',
-            // pointRadius: 5,
-            // borderWidth: 1,
-            // lineTension: 0
+        credits: {
+            enabled: false
         },
-        {
-            data: [16, 7, 14, 7, 7, 5, 15, 12, 11, 8, 14],
-            backgroundColor: '#FF7A00',
-            // borderColor: '#3A8282',
-            // pointRadius: 5,
-            // borderWidth: 1,
-            // lineTension: 0
-        }],
-    },
-    options: {
-        responsive: true,
-
-        scales: {
-            xAxes: [{
-                gridLines: {
-                    borderDash: [2, 2],
-                    color: '#E7E7E7',
-                },
-                barThickness: 5,
-            }],
-            yAxes: [{
-                ticks: {
-                    suggestedMin: 0,
-                    suggestedMax: 25,
-                },
-                gridLines: {
-                    borderDash: [2, 2],
-                    color: '#CBCBCB'
-                },
-            }]
-        },
+        title: false,
         legend: {
-            display: false,
+            enabled: false
         },
-        tooltips: {
-            mode: 'label',
-            backgroundColor: '#3A8282',
-        }
-    }
+        colors: ['#6F52ED', '#FF7A00'],
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Sep', 'Oct', 'Nov', 'Dec'],
+        },
+        yAxis: {
+            title: false,
+            gridLineDashStyle: 'LongDash',
+            max: 23,
+            gridLineWidth: 1,
+        },
+        plotOptions: {
+            column: {
+                groupPadding: 0.3,
+                pointWidth: 5,
+                borderWidth: 0,
+                borderRadius: 3,
+            }
+        },
+        series: [{
+            name: 'Applied',
+            data: [19, 15, 24, 8, 11, 10, 18, 19, 12, 17, 23],
+
+        }, {
+            name: 'Left',
+            data: [16, 7, 14, 7, 7, 5, 15, 12, 11, 8, 14],
+        }]
+    });
+
+    let donutChart = Highcharts.chart('donutChart', {
+        chart: {
+            type: 'pie',
+            spacingBottom: 0,
+            spacingTop: 0,
+            spacingLeft: 0,
+            spacingRight: 0,
+        },
+        credits: {
+            enabled: false
+        },
+        title: false,
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            pie: {
+                innerSize: '85%',
+                dataLabels: {
+                    enabled: false
+                }
+            },
+            series: {
+                states: {
+                    hover: {
+                        enabled: false
+                    }
+                },
+                enableMouseTracking: false
+            }
+        },
+        tooltip: {
+            enabled: false
+        },
+        colors: ['#6F52ED', '#FFB800', '#FF4C61', '#33D69F'],
+        series: [{
+            data: [20, 20, 15, 7]
+        }]
+    });
 });
